e6f3fc232585b58575a461e8a6256d23d30643d0
Braian Piñero <braian199198@gmail.com>
2024-08-19 20:26:22 -0400

    Modificacion

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("¡Gracias por tu mensaje! Me pondré en contacto contigo pronto.");
+});
